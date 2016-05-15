655626828dd68b12b54fb2b2c32e86805d93c77d
Michael Gilliland <mjg.py3@gmail.com>
2016-05-15 11:27:33 -0400

    Build tokenizer

--- src/tokenizer.exs
+++ src/tokenizer.exs
@@ -1,1 +1,40 @@
-IO.puts "Hello World"
+defmodule Tokenizer do
+  def tokenize(text) do
+    text
+      |> String.codepoints
+      |> dotokenize
+  end
+
+  defp dotokenize(["\\" | rest]) do
+    [{:lambda, nil} | dotokenize(rest)]
+  end
+
+  defp dotokenize(["." | rest]) do
+    [{:dot, nil} | dotokenize(rest)]
+  end
+
+  defp dotokenize(["(" | rest]) do
+    [{:lparen, nil} | dotokenize(rest)]
+  end
+
+  defp dotokenize([")" | rest]) do
+    [{:rparen, nil} | dotokenize(rest)]
+  end
+
+  defp dotokenize([c | rest]) do
+    if String.contains?("abcdefghijklmnopqrstuvwxkz", c) do
+      [{:name, c} | dotokenize(rest)]
+    else
+      dotokenize(rest)
+    end
+  end
+
+  defp dotokenize([]) do
+    []
+  end
+
+end
+
+#IO.puts inspect(Tokenizer.tokenize("\\\\"))
+#IO.puts inspect(Tokenizer.tokenize("\\."))
+#IO.puts inspect(Tokenizer.tokenize("(\\x . x \\y . y)"))
